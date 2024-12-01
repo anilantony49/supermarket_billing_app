08daa7471368d7dd99837991cfc2ef0d51a525b7
Anil Antony <anilvithayathil007@gmail.com>
2024-12-01 12:57:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# machine_task_atts
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/machine_task_atts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.machine_task_atts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
